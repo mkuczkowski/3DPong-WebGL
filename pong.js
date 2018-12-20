e902ae7aa1ccd7ce38ebc15d6324f944f6d30a11
mkuczkowski <incogniotoo@gmail.com>
2018-12-20 15:17:55 +0100

    Player movement & ball physics added

--- pong.js
+++ pong.js
@@ -5,7 +5,9 @@ function initialize() {
 
 // --- runs every frame ---
 function draw() {
-    renderer.render(scene, camera); 
+    renderer.render(scene, camera);
+    ballMovement();
+    playerMovement();
     requestAnimationFrame(draw);
 }
 
@@ -43,11 +45,16 @@ pointLight.distance = 10000;
 
 scene.add(pointLight);
 
-let planeMaterial = new THREE.MeshLambertMaterial({color: 0x004dff});
+const court = new THREE.TextureLoader().load( "https://i.imgur.com/nWDveBQ.png" );
+
+let planeMaterial = new THREE.MeshBasicMaterial({map: court});
+
+const COURT_WIDTH = 390;
+const COURT_HEIGHT = 180;
 
 let gameArea = new THREE.Mesh(
     new THREE.PlaneGeometry(
-        370, 180, 10, 10
+        COURT_WIDTH, COURT_HEIGHT, 10, 10
     ),
     planeMaterial
 );
@@ -71,8 +78,69 @@ let player2 = new THREE.Mesh(
 
 scene.add(player2);
 
-player1.position.x = -370/2 + 8;
-player2.position.x = 370/2 - 8;
+player1.position.x = -COURT_WIDTH/2 + 8;
+player2.position.x = COURT_WIDTH/2 - 8;
 
 player1.position.z = 8;
 player2.position.z = 8;
+
+const BALL_SPEED = 1.7;
+let ballDirectionY = 1;
+let ballDirectionX = 1;
+
+function ballMovement() {
+    ball.position.x -= ballDirectionX * BALL_SPEED;
+    ball.position.y -= ballDirectionY * BALL_SPEED;
+
+    if(ball.position.y <= -COURT_HEIGHT/2) ballDirectionY = -ballDirectionY;
+    if(ball.position.y >= COURT_HEIGHT/2)  ballDirectionY = -ballDirectionY;
+
+    if (ball.position.x <= -COURT_WIDTH/2) {
+        //player2 scores
+        nextRound();
+    }
+    if (ball.position.x >= COURT_WIDTH/2) {
+        //player1 scores
+        nextRound();
+    }
+
+    if (ball.position.x <= player1.position.x + 8
+        &&  ball.position.x >= player1.position.x) {
+            if (ball.position.y <= player1.position.y + 45/2
+            &&  ball.position.y >= player1.position.y - 45/2) {               
+                    ballDirectionX = -ballDirectionX;   
+            }
+        }
+
+    if (ball.position.x <= player2.position.x + 8
+        &&  ball.position.x >= player2.position.x) {
+            if (ball.position.y <= player2.position.y + 45/2
+            &&  ball.position.y >= player2.position.y - 45/2) {
+                    ballDirectionX = -ballDirectionX;
+                }
+            }
+            
+}
+
+function playerMovement() {
+    document.onkeydown = function(e) {
+        switch (e.keyCode) {
+            case 37: //left arrow
+                if(player1.position.y < COURT_HEIGHT / 2 - 25) 
+                    player1.position.y += 3.5;   
+                break;
+            case 39: //right arrow
+                if(player1.position.y > -COURT_HEIGHT / 2 + 25) 
+                    player1.position.y -= 3.5;
+                break;
+        }
+    };
+    player2.position.y = ball.position.y * 0.7;
+}
+
+function nextRound() {
+    ball.position.x = 0;
+    ball.position.y = 0;
+    player1.position.y = 0;
+    player2.position.y = 0;
+}
